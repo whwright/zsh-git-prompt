5e9c548c07b151d3db4a7a8aab281c4d7962ec78
userName <userEmail>
2014-07-24 20:25:11 -0400

    Spacing fixed.

--- zshrc.sh
+++ zshrc.sh
@@ -18,16 +18,16 @@ add-zsh-hook precmd precmd_update_git_vars
 ## Function definitions
 function preexec_update_git_vars() {
     case "$2" in
-	git*|hub*|gh*|stg*)
-	__EXECUTED_GIT_COMMAND=1
-	;;
+    git*|hub*|gh*|stg*)
+        __EXECUTED_GIT_COMMAND=1
+        ;;
     esac
 }
 
 function precmd_update_git_vars() {
     if [ -n "$__EXECUTED_GIT_COMMAND" ] || [ -n "$ZSH_THEME_GIT_PROMPT_NOCACHE" ]; then
-	update_current_git_vars
-	unset __EXECUTED_GIT_COMMAND
+        update_current_git_vars
+        unset __EXECUTED_GIT_COMMAND
     fi
 }
 
@@ -41,48 +41,48 @@ function update_current_git_vars() {
     local gitstatus="$__GIT_PROMPT_DIR/gitstatus.py"
     _GIT_STATUS=`python ${gitstatus}`
     __CURRENT_GIT_STATUS=("${(@f)_GIT_STATUS}")
-	GIT_BRANCH=$__CURRENT_GIT_STATUS[1]
-	GIT_REMOTE=$__CURRENT_GIT_STATUS[2]
-	if [[ "." == "$GIT_REMOTE" ]]; then
-	    unset GIT_REMOTE
-	fi
-	GIT_STAGED=$__CURRENT_GIT_STATUS[3]
-	GIT_CONFLICTS=$__CURRENT_GIT_STATUS[4]
-	GIT_CHANGED=$__CURRENT_GIT_STATUS[5]
-	GIT_UNTRACKED=$__CURRENT_GIT_STATUS[6]
-	GIT_STASHED=$__CURRENT_GIT_STATUS[7]
-	GIT_CLEAN=$__CURRENT_GIT_STATUS[8]
+    GIT_BRANCH=$__CURRENT_GIT_STATUS[1]
+    GIT_REMOTE=$__CURRENT_GIT_STATUS[2]
+    if [[ "." == "$GIT_REMOTE" ]]; then
+        unset GIT_REMOTE
+    fi
+    GIT_STAGED=$__CURRENT_GIT_STATUS[3]
+    GIT_CONFLICTS=$__CURRENT_GIT_STATUS[4]
+    GIT_CHANGED=$__CURRENT_GIT_STATUS[5]
+    GIT_UNTRACKED=$__CURRENT_GIT_STATUS[6]
+    GIT_STASHED=$__CURRENT_GIT_STATUS[7]
+    GIT_CLEAN=$__CURRENT_GIT_STATUS[8]
 }
 
 
 git_super_status() {
     precmd_update_git_vars
     if [ -n "$__CURRENT_GIT_STATUS" ]; then
-	STATUS="$ZSH_THEME_GIT_PROMPT_PREFIX$ZSH_THEME_GIT_PROMPT_BRANCH$GIT_BRANCH%{${reset_color}%}"
-	if [ -n "$GIT_REMOTE" ]; then
-            STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_REMOTE$GIT_REMOTE%{${reset_color}%}"
-	fi
-	STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_SEPARATOR"
-	if [ "$GIT_STAGED" -ne "0" ]; then
-	    STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_STAGED$GIT_STAGED%{${reset_color}%}"
-	fi
-	if [ "$GIT_CONFLICTS" -ne "0" ]; then
-	    STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_CONFLICTS$GIT_CONFLICTS%{${reset_color}%}"
-	fi
-	if [ "$GIT_CHANGED" -ne "0" ]; then
-	    STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_CHANGED$GIT_CHANGED%{${reset_color}%}"
-	fi
-	if [ "$GIT_UNTRACKED" -ne "0" ]; then
-	    STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_UNTRACKED$GIT_UNTRACKED%{${reset_color}%}"
-	fi
-	if [ "$GIT_STASHED" -ne "0" ]; then
-	    STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_STASHED$GIT_STASHED%{${reset_color}%}"
-	fi
-	if [ "$GIT_CLEAN" -eq "1" ]; then
-	    STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_CLEAN"
-	fi
-	STATUS="$STATUS%{${reset_color}%}$ZSH_THEME_GIT_PROMPT_SUFFIX"
-	echo "$STATUS"
+        STATUS="$ZSH_THEME_GIT_PROMPT_PREFIX$ZSH_THEME_GIT_PROMPT_BRANCH$GIT_BRANCH%{${reset_color}%}"
+    if [ -n "$GIT_REMOTE" ]; then
+        STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_REMOTE$GIT_REMOTE%{${reset_color}%}"
+    fi
+        STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_SEPARATOR"
+    if [ "$GIT_STAGED" -ne "0" ]; then
+        STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_STAGED$GIT_STAGED%{${reset_color}%}"
+    fi
+    if [ "$GIT_CONFLICTS" -ne "0" ]; then
+        STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_CONFLICTS$GIT_CONFLICTS%{${reset_color}%}"
+    fi
+    if [ "$GIT_CHANGED" -ne "0" ]; then
+        STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_CHANGED$GIT_CHANGED%{${reset_color}%}"
+    fi
+    if [ "$GIT_UNTRACKED" -ne "0" ]; then
+        STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_UNTRACKED$GIT_UNTRACKED%{${reset_color}%}"
+    fi
+    if [ "$GIT_STASHED" -ne "0" ]; then
+        STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_STASHED$GIT_STASHED%{${reset_color}%}"
+    fi
+    if [ "$GIT_CLEAN" -eq "1" ]; then
+        STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_CLEAN"
+    fi
+        STATUS="$STATUS%{${reset_color}%}$ZSH_THEME_GIT_PROMPT_SUFFIX"
+    echo "$STATUS"
     fi
 }
 
